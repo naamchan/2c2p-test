25d9b665dd092131a542fa6f3beaa9bf5bb96654
Thara Mongkolwannalert <naamchan@gmail.com>
2021-01-17 11:34:44 +0000

    - fix: potential sql injection

---
+++ Models/ResultTransactionModel.cs
@@ -0,0 +1,49 @@
+#nullable enable
+
+using System.Text.Json.Serialization;
+
+namespace _2c2p_test.Model
+{
+    public record ResultTransactionModel
+    {
+        [JsonPropertyName("id")]
+        public string ID { get; init; }
+
+        [JsonPropertyName("payment")]
+        public string Payment { get; init; }
+
+        [JsonPropertyName("status")]
+        public string Status { get; init; }
+
+        public ResultTransactionModel(string id, string payment, string status)
+        {
+            ID = id;
+            Payment = payment;
+            Status = status;
+        }
+
+        public ResultTransactionModel(TransactionModel transactionModel)
+        {
+            ID = transactionModel.TransactionID;
+            Payment = $"{transactionModel.Amount} {transactionModel.CurrencyCode}";
+            Status = ConvertStatus(transactionModel.Status);
+
+            System.Console.WriteLine($"{ID} {Payment} {Status}");
+        }
+
+        private string ConvertStatus(TransactionModel.TransactionStatus status)
+        {
+            switch (status)
+            {
+                case TransactionModel.TransactionStatus.Approved:
+                    return "A";
+                case TransactionModel.TransactionStatus.Finished:
+                    return "R";
+                case TransactionModel.TransactionStatus.Failed:
+                    return "D";
+                default:
+                    return "N/A";
+            }
+        }
+    }
+}
--- Repositories/TransactionRepository.cs
+++ Repositories/TransactionRepository.cs
@@ -5,34 +5,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using _2c2p_test.Exceptions;
 using _2c2p_test.Model;
-using _2c2p_test.Services;
 using MySql.Data.MySqlClient;
 
 namespace _2c2p_test.Repository
 {
     public class TransactionRepository : RepositoryBase<TransactionModel>
     {
-        private readonly IEnumerable<TransactionModel>? models;
+        public TransactionRepository(IServiceProvider serviceProvider) : base(serviceProvider) { }
 
-        public TransactionRepository(IServiceProvider serviceProvider, IEnumerable<TransactionModel>? models = null) : base(serviceProvider)
-        {
-            this.models = models;
-        }
-
-        public override async Task<bool> Save()
+        public override async Task<bool> Save(IEnumerable<TransactionModel>? models)
         {
             if (models is null || !models.Any())
             {
                 return false;
             }
 
-            var mysqlService = serviceProvider.GetService(typeof(MySQLService)) as MySQLService;
-            if (mysqlService is null)
-            {
-                throw new CannotFindServiceException("MySQL");
-            }
+            var mysqlService = GetMySQLService();
             using var connection = mysqlService.GetConnection();
 
             StringBuilder queryText = new("INSERT INTO `transactions` (`id`, `amount`, `currency_code`, `transaction_date`, `status`) VALUES ");
@@ -55,21 +44,50 @@ namespace _2c2p_test.Repository
 
             await connection.OpenAsync();
             using var command = new MySqlCommand(queryText.ToString(), connection);
-            var c = await command.ExecuteNonQueryAsync();
-            System.Console.WriteLine(c);
+            await command.ExecuteNonQueryAsync();
 
             return true;
+        }
+
+        public async Task<IEnumerable<TransactionModel>> FetchByCurrencyCode(string currencyCode)
+        {
+            var mysqlService = GetMySQLService();
+            using var connection = mysqlService.GetConnection();
+
+            var queryText = "SELECT `id`, `amount`, `currency_code`, `transaction_date`, `status` FROM `transactions` where `currency_code`=?currencyCode";
+
+            await connection.OpenAsync();
+            using var command = new MySqlCommand(queryText.ToString(), connection);
+            command.Parameters.Add(new MySqlParameter("?currencyCode", currencyCode));
+
+            using var cursor = await command.ExecuteReaderAsync();
+            List<TransactionModel> models = new();
+
+            while (await cursor.ReadAsync())
+            {
+                models.Add(new TransactionModel(
+                    transactionID: Convert.ToString(cursor["id"]) ?? "N/A",
+                    amount: Convert.ToDecimal(cursor["amount"]),
+                    currencyCode: Convert.ToString(cursor["currency_code"]) ?? "N/A",
+                    transactionDate: Convert.ToDateTime(cursor["transaction_date"]),
+                    transactionStatus: (TransactionModel.TransactionStatus)Convert.ToInt32(cursor["status"])
+                ));
+            }
+
+            return models;
         }
 
         private void Append(StringBuilder queryText, TransactionModel model)
         {
             queryText.Append("(")
-                .Append("'").Append(model.TransactionID).Append("',")
-                .Append("'").Append(model.Amount.ToString()).Append("',")
-                .Append("'").Append(model.CurrencyCode).Append("',")
+                .Append("'").Append(MySqlHelper.EscapeString(model.TransactionID)).Append("',")
+                .Append("'").Append(MySqlHelper.EscapeString(model.Amount.ToString())).Append("',")
+                .Append("'").Append(MySqlHelper.EscapeString(model.CurrencyCode)).Append("',")
                 .Append("'").Append(model.TransactionDate.ToString("o")).Append("',")
                 .Append("'").Append(((int)model.Status)).Append("'")
                 .Append(")");
         }
+
+
     }
 }
